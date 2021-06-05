d1e5f7ab537d40e76f96087fa27387fa71c74f26
vikas <vikas@host.docker.internal>
2021-06-06 05:09:01 +0530

    first commit

---
+++ src/main/java/com/app/add/Add.java
@@ -0,0 +1,11 @@
+package com.app.add;
+
+public class Add {
+	public static void m1(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public static void main(String[] args) {
+		m1(5, 2);
+	}
+}
